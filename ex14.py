acbf8467939b0a2768e7af1d239e2da155bd4a34
Paulo Higa <paulojhiga@gmail.com>
2026-03-20 19:51:49 -0300

    ex
    criação do ex15
    mudança no ex14

--- ex14.py
+++ ex14.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 inicio = time.time()
 
@@ -8,4 +9,7 @@ for i in range(10000000):
     x = 2 * i
     Soma += x
 fim = time.time()
+
+print(f"Inicio: {datetime.fromtimestamp(inicio)}")
+print(f"Fim: {datetime.fromtimestamp(fim)}")
 print(f"Tempo: {fim - inicio:.4f} segundos")
